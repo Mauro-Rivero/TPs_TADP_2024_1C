d8d23ce4290d8c1b5292fa430a220af4952a727d
Federico Waldman <fwaldman@frba.utn.edu.ar>
2024-04-30 17:40:51 -0300

    Comentarios

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -1,17 +1,20 @@
 class Interceptor
-  attr_accessor :procBefore, :procAfter, :clase
 
-  def initialize(procBefore, procAfter, clase)
-    @procBefore = procBefore
-    @procAfter = procAfter
-    @clase = clase
+  def agregarBefore(procBefore)
+    @procsBefore ||= []
+    @procsBefore.push(procBefore)
+    end
+
+  def agregarAfter(procAfter)
+    @procsAfter ||= []
+    @procsAfter.push(procAfter)
   end
 
-  def before_and_after_each_call(name, *args)
-    @procBefore.call
-    @clase.send(name, args)
-    @procAfter.call
-
+  def procBefore()
+    proc {@procsBefore.map { |procBefore| procBefore.call}}
+    end
+  def procAfter()
+    proc {@procsAfter.map { |procAfter| procAfter.call}}
   end
 end
 
@@ -22,23 +25,25 @@ class Module
     @invariantes.push(block)
   end
   def before_and_after_each_call(procBefore, procAfter)
-    @interceptor = Interceptor.new(procBefore, procAfter, self)
+    @interceptor ||= Interceptor.new
+    @interceptor.agregarBefore(procBefore)
+    @interceptor.agregarAfter(procAfter)
   end
 
 #proc "intermedio" que chequee la invariante recibida, before and after, y que en caso de no cumplirse, tirar el error.
 
-  def method_missing(name, *args)
-    @interceptor.before_and_after_each_call(name, args)
-    super
-  end
 #hablado en checkpoint
   def method_added(name)
     unbound_method = self.instance_method(name)
-    seSobreescribio = false
-    define_method(name) do
-      @interceptor.before
-      unbound_method.bind(self).call
-      @interceptor.after
+    if !@seSobreescribio
+      @seSobreescribio = true
+      define_method(name) do
+        @interceptor.procBefore.call
+        unbound_method.bind(self).call
+        @interceptor.procAfter.call
+      end
+    else
+      @seSobreescribio = false
     end
   end
 
@@ -46,15 +51,15 @@ end
 
 #usar attr accesor y metodos de atributos
 #esto no haria falta si el initialize esta contem´lado con el method added
-class Class
-  def new(args, &block)
-    # Tu implementación personalizada del método new aquí
-    instancia = allocate  # Crea una nueva instancia sin llamar a initialize
-    instancia.initialize(args, &block)  # Llama a initialize con los argumentos proporcionados
-    @invariantes.map { |invariant| instancia.instance_eval(invariant)}
-    return instancia
-  end
-end
+# class Class
+#   def new(args, &block)
+#     # Tu implementación personalizada del método new aquí
+#     instancia = allocate  # Crea una nueva instancia sin llamar a initialize
+#     instancia.initialize(args, &block)  # Llama a initialize con los argumentos proporcionados
+#     @invariantes.map { |invariant| instancia.instance_eval(invariant)}
+#     return instancia
+#   end
+# end
 
 class Guerrero
   before_and_after_each_call(proc{puts "entre"},proc { puts "sali"})
@@ -65,5 +70,10 @@ class Guerrero
   def hablar
     puts "hola"
   end
+
 end
-metodo(&block)
+
+pepe = Guerrero.new
+
+pepe.hablar
+
