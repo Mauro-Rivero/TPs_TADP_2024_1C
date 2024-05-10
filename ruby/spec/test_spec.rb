51bd8f077dd07ee06170878de48b51b4004ea57d
Federico Waldman <fwaldman@frba.utn.edu.ar>
2024-05-09 23:41:10 -0300

    Tests sobre ejemplo

--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,103 +1,72 @@
 require 'rspec'
 require_relative '../lib/prueba.rb'
 describe 'Prueba' do
-    it 'debería pasar este test' do
-      class Persona
-        extend Contrato
-        attr_accessor :algo
+  class Pila
+    extend Contrato
+    attr_accessor :current_node, :capacity
 
-        invariant{ algo > 4 }
-        invariant{ algo > 0 && algo < 100 }
-        #invariant{ algo < 0}
+    #AGREGAMOS ESTO PARA CHEQUEAR QUE FUNCIONE TAMBIÉN
+    before_and_after_each_call(proc{puts"Entre a un metodo"}, proc{puts"Sali de un metodo"})
 
-        before_and_after_each_call(
-          proc{@algo = @algo +5},
-          proc{@algo = @algo - 3}
-        )
-        before_and_after_each_call(
-          proc{puts"hola"},
-          proc{puts"chau"}
-        )
-        before_and_after_each_call(
-          proc{puts"hola"},
-          proc{puts"chau"}
-        )
+    invariant { capacity >= 0 }
 
-        def initialize(algo)
-          @algo = algo
-        end
-
-        pos {algo > 1}
-        def hablar()
-          @algo = @algo-1
-        end
-
-        pre { algo >= 0 }
-        pos { algo >= 0 }
-        def morir()
-          puts "MORI"
-        end
-      end
-
-      pepe = Persona.new(5)
-      pepe.hablar
-      puts "nuevo metodo"
-      pepe.morir
-
-      # expect(pepe.algo).to eq(3)
-      # pepe.hablar
-      # expect(pepe.algo).to eq(6)
+    pos { empty? }
+    def initialize(capacity)
+      @capacity = capacity
+      @current_node = nil
     end
 
-    it 'ejemplo' do
-      class Pila
-        extend Contrato
-        attr_accessor :current_node, :capacity
-
-        invariant { capacity >= 0 }
-
-        pos { empty? }
-        def initialize(capacity)
-          @capacity = capacity
-          @current_node = nil
-        end
-
-        pre { !full? }
-        pos { height > 0 }
-        def push(element)
-          @current_node = Node.new(element, current_node)
-        end
-
-        pre { !empty? }
-        def pop
-          element = top
-          @current_node = @current_node.next_node
-          element
-        end
-
-        pre { !empty? }
-        def top
-          current_node.element
-        end
-
-        def height
-          empty? ? 0 : current_node.size
-        end
-
-        def empty?
-          current_node.nil?
-        end
-
-        def full?
-          height == capacity
-        end
-
-        Node = Struct.new(:element, :next_node) do
-          def size
-            next_node.nil? ? 1 : 1 + next_node.size
-          end
-        end
-      end
-
+    pre { !full? }
+    pos { height > 0 }
+    def push(element)
+      @current_node = Node.new(element, current_node)
     end
+
+    pre { !empty? }
+    def pop
+      element = top
+      @current_node = @current_node.next_node
+      element
+    end
+
+    pre { !empty? }
+    def top
+      current_node.element
+    end
+
+    def height
+      empty? ? 0 : current_node.size
+    end
+
+    def empty?
+      current_node.nil?
+    end
+
+    def full?
+      height == capacity
+    end
+
+    Node = Struct.new(:element, :next_node) do
+      def size
+        next_node.nil? ? 1 : 1 + next_node.size
+      end
+    end
+  end
+  it 'deberia fallar invariante' do
+    expect{Pila.new(-2)}.to raise_error RuntimeError
+  end
+
+  it 'deberia crearse una pila' do
+    expect(Pila.new(2).class).to be(Pila)
+  end
+
+  it 'deberia fallar precondición' do
+    pila = Pila.new(0)
+    expect{pila.push(1)}.to raise_error RuntimeError
+  end
+
+  it 'deberia fallar precondición' do
+    pila = Pila.new(0)
+    expect{pila.top}.to raise_error RuntimeError
+  end
 end
