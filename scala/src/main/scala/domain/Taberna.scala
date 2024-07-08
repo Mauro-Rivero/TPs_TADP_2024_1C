4b5b0fe95287676ec120685d66b013e599fd0fc5
Mauro Rivero <marivero@frba.utn.edu.ar>
2024-07-08 18:36:20 -0300

    Pequeñas agregaciones

--- scala/src/main/scala/domain/Taberna.scala
+++ scala/src/main/scala/domain/Taberna.scala
@@ -1,21 +1,20 @@
 package domain
 
-object Taberna extends App{
-  var tablon: List[Mision] = List()
-  
+case class Taberna (var tablon: List[Mision] = List()){
   def elegirMisionPara(equipo: Equipo, criterio: Criterio): Option[Mision] = {
     val mejorMision = tablon.reduceLeft((unaMision, otraMision) => if (criterio(equipo.realizar(unaMision), equipo.realizar(otraMision))) unaMision else otraMision)
     Some(mejorMision).filter(equipo.puedeRealizar) //Si no puede realizar queda vacia la monada y devuelve None
   }
 
-//  def entrenar(equipo:Equipo):Equipo = {
-//  ???
-//  }
-//
-//  
-//  equipo = entrenar(equipo)
-}
-//Entrenar: Cuando un equipo entrena, intenta realizar todas las misiones, una por una,
-//eligiendo la mejor misión para hacer a continuación. Cada misión se realiza luego de haber cobrado
+//  Entrenar: Cuando un equipo entrena, intenta realizar todas las misiones, una por una,
+//  eligiendo la mejor misión para hacer a continuación. Cada misión se realiza luego de haber cobrado
 //  la recompensa de la anterior y el equipo no se detiene hasta haber finalizado todas las misiones o
 //  fallar una.
+
+def entrenar(equipo:Equipo, criterio: Criterio, tablon: List[Mision] = this.tablon):(Equipo, List[Mision]) = { //agrego criterio pues dice que elige la mejor mision.. Segun qué?
+    elegirMisionPara(equipo, criterio) match {
+      case Some(mision) => entrenar(equipo.realizar(mision), criterio, this.sacarMisionDeTablon(mision))
+      case None => (equipo, tablon)
+    }
+  }
+}
--- scala/src/main/scala/domain/Trabajo.scala
+++ scala/src/main/scala/domain/Trabajo.scala
@@ -2,9 +2,7 @@ package domain
 
 // debe ser una trait?
 class Trabajo(val stats: Stats, val statPrincipal: StatPrincipal) {
-  
 }
-
 
 val Guerrero = new Trabajo(new Stats(hp = 10, fuerza = 15, inteligencia = -10), Fuerza)
 val Mago = new Trabajo(new Stats(fuerza = -20, inteligencia = 20), Inteligencia)
