c1bac8479e420e3dc4d159e4715e06cc42ff4c2c
Federico Waldman <fwaldman@frba.utn.edu.ar>
2024-07-11 21:20:37 -0300

    Update Equipo.scala

--- scala/src/main/scala/domain/Equipo.scala
+++ scala/src/main/scala/domain/Equipo.scala
@@ -1,28 +1,18 @@
 package domain
 
-
 case class Equipo(nombre: String, miembros: Set[Heroe] = Set(), pozoComun: Int = 0){
 
   def mejorHeroeSegun(criterio: Cuantificador): Option[Heroe] =  miembros.maxByOption(criterio) //mejorHeroeSegunOptional....
-  def mejorHeroeSegunOptional(criterio: Cuantificador): Option[Heroe] =  miembros.maxByOption(criterio)
-
-  def obtenerItem(item:Item): Equipo = {
-    val heroeACambiar = mejorHeroeSegun(heroe => heroe.equiparItem(item).statPrincipal() -  heroe.statPrincipal()) // al no tener efecto sabemos que heroe es pero sin modificarlo
-    heroeACambiar match {
-      case Some(heroe) if heroe.esPositivoEquipar(item) =>  reemplazarMiembro(heroe, heroe.equiparItem(item))
-      case _ => vender(item)
-    }
-  }
-  /* OPCION DE CORRECCION FUNCION OBTENERITEM() DE ARRIBA
+  def mejorHeroeSegunOptional(criterio: CuantificadorOptional): Option[Heroe] =  miembros.maxByOption(miembro => criterio(miembro).getOrElse(Int.MinValue))
+  
   def obtenerItem(item : Item): Equipo = {
     val heroeACambiar = mejorHeroeSegun(heroe => heroe.equiparItem(item).statPrincipal() -  heroe.statPrincipal())
-    if(heroeACambiar.exists(heroe => heroe.esPositivoEquipar(item))){
-      heroeACambiar.map(heroe => reemplazarMiembro(heroe, heroe.equiparItem(item))).getOrElse(this) //No deberia llegar nunca al getOrElse(this)
+    if(heroeACambiar.exists(_.esPositivoEquipar(item))){
+      reemplazarMiembro(heroeACambiar.get, heroeACambiar.get.equiparItem(item))
     }else{
       vender(item)
     }
   }
-*/
   def vender( item: Item): Equipo = this.copy(pozoComun = pozoComun + item.precio)
 
   def obtenerMiembro(heroe: Heroe): Equipo = this.copy(miembros = miembros + heroe)
@@ -30,41 +20,33 @@ case class Equipo(nombre: String, miembros: Set[Heroe] = Set(), pozoComun: Int =
   def eliminarMiembro(heroe: Heroe): Equipo = this.copy(miembros = miembros - heroe)
 
   def reemplazarMiembro(unMiembro: Heroe, otroMiembro: Heroe): Equipo = eliminarMiembro(unMiembro).obtenerMiembro(otroMiembro)
+  
+  def lider(): Option[Heroe] = {
+    val mejor = mejorHeroeSegun(_.statPrincipal())
+    mejor.filter { lider =>
+      eliminarMiembro(lider).mejorHeroeSegun(_.statPrincipal()).exists(_.statPrincipal() != lider.statPrincipal())
+    }
+  }
 
-  def lider(): Option[Heroe] = {
-    val mejor = mejorHeroeSegun(_.statPrincipal())
-    mejor.flatMap { lider =>
-      val segundo = eliminarMiembro(lider).mejorHeroeSegun(_.statPrincipal())
-      segundo match {
-        case Some(segundoLider) if lider.statPrincipal() == segundoLider.statPrincipal() => None
-        case _ => Some(lider)
-      }
-    }
-  }
-/* OPCION DE CORRECCION FUNCION LIDER() DE ARRIBA
-  def lider(): Option[Heroe] = {
-    val mejor = mejorHeroeSegun(_.statPrincipal())
-    mejor.flatMap { lider =>
-      val segundo = eliminarMiembro(lider).mejorHeroeSegun(_.statPrincipal())
-      if(segundo.exists(segundoLider => lider.statPrincipal() == segundoLider.statPrincipal())){
-        None
-      }else {
-        Some(lider)
-      }
-    }
-  }
-*/
   def realizar(mision: Mision): Equipo = {
     tratarDeRealizarTareas(mision.tareas) match {
       case None => this
       case Some(equipo) => mision.recompensa(equipo)
     }
   }
- /* OPCION DE CORRECCION FUNCION realizar(mision: Mision) DE ARRIBA
-  def realizar(mision: Mision): Equipo = {
+
+  def realizar2(mision: Mision): Equipo = {
+    val result = tratarDeRealizarTareas(mision.tareas)
+    if (result.nonEmpty) {
+      mision.recompensa(result.get)
+    } else {
+      this
+    }
+  }
+  def realizar3(mision: Mision): Equipo = {
     tratarDeRealizarTareas(mision.tareas).map(equipo => mision.recompensa(equipo)).getOrElse(this)
   }
-  */
+  
   def puedeRealizar(mision: Mision): Boolean = tratarDeRealizarTareas(mision.tareas).isDefined
   
   private def tratarDeRealizarTareas(tareas: List[Tarea]): Option[Equipo] = {
@@ -75,8 +57,8 @@ case class Equipo(nombre: String, miembros: Set[Heroe] = Set(), pozoComun: Int =
   }
 
   private def tratarDeRealizarTarea(tarea:Tarea): Option[Equipo] = {
-    if(tarea.condicionDeRealizacion(this)){ //ES OPTIONAL
-      mejorHeroeSegun(heroe => tarea.facilidad(heroe)).map { heroe =>
+    if(tarea.condicionDeRealizacion.exists(_(this))){ //ES OPTIONAL
+      mejorHeroeSegunOptional(heroe => tarea.facilidad(heroe)).map { heroe =>
           reemplazarMiembro(heroe, heroe.realizar(tarea))
         }
     }else{
